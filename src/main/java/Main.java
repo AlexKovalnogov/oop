8249c2d2e92859262a57fcf80adfd2b2953595b1
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2019-01-17 13:44:11 +0200

    improvement 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,25 +8,25 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-      /*  Client client = new Client();
-        Administrator administrator = new Administrator();
+        System.out.println("Input personal position:director,admin,serviceman,client.");
         Appliance coffeemachine = new Appliance(24.5, "coffeemachine", "24/12.15");
-
-        client.giveApplianceIntoRepair(coffeemachine, administrator);
-         administrator.getApplianceFromRepair();*/
         try {
-        Position  position=Position.valueOf(scanner.nextLine().toUpperCase());
+        StuffPosition position= StuffPosition.valueOf(scanner.nextLine().toUpperCase());
 
             switch (position) {
-                case DIRECTOR: {
+                case DIRECTOR:
                     System.out.println("1");
                     break;
-                }
 
-                case CLIENT: {
+                case CLIENT:
                     System.out.println("2");
+                    Client client = new Client();
+
+                    Administrator administrator = new Administrator();
+                    client.giveApplianceIntoRepair(coffeemachine, administrator);
+                    administrator.getApplianceFromRepair();
                     break;
-                }
+
                 case ADMIN:
                     System.out.println("3");
                     break;
--- src/main/java/StuffPosition.java
+++ src/main/java/StuffPosition.java
@@ -1,4 +1,4 @@
-public enum Position {
+public enum StuffPosition {
     DIRECTOR,
     ADMIN,
     CLIENT,
